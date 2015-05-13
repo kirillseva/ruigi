8715028dbd3a1fc64b4444d2059e49f250373ead
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-05-13 11:01:15 -0500

    refactor R6 definitions

--- R/ruigi_node.R
+++ R/ruigi_node.R
@@ -1,3 +1,32 @@
+`ruigi_node&initialize` <- function(name, requires, runner, target) {
+  if (!missing(name)) self$name <- name
+  if (!missing(requires)) {
+    if (length(requires) > 0) {
+      if (!all(sapply(requires, is.ruigi_target)))
+        stop("Invalid ", sQuote("requires"), " for a ruigi node")
+    }
+    self$requires <- requires
+  }
+  ## A runner is the computation that the node must produce
+  ## In code, a runner is a function that has two arguments:
+  ## *requires*, that is a list of ruigi_nodes, and *produces*, which is a ruigi_target
+  ## In practise, this function will be called with the appropriate values by
+  ## the scheduler.
+  if (missing(runner)) stop("Every node must have a ", sQuote("runner"))
+  if (!is.function(runner) | length(runner) != 1)
+    stop("A ", sQuote("runner"), " must be a function")
+  if (! all(names(formals(runner)) %in% c("requires", "target")))
+    stop("A ", sQuote("runner"), " must take ", sQuote("requires"), "and ",
+    sQuote("target"), " as inputs")
+  self$runner <- runner
+  if (missing(target)) stop("Every node must have a ", sQuote("target"))
+  if (!is.ruigi_target(target) | length(target) != 6) {
+    browser()
+    stop("A node must have one and only one target that is a ", sQuote("ruigi_target"))
+  }
+  self$target <- target
+}
+
 #' @export
 ruigi_node <- R6::R6Class("ruigi_node",
   public = list(
@@ -10,34 +39,7 @@ ruigi_node <- R6::R6Class("ruigi_node",
     ## since this is the primary interface with the user.
     ## It is important to check early because execution of the node may happen
     ## on another machine, or it will start late and cause errors in a huge batch job.
-    initialize = function(name, requires, runner, target) {
-      if (!missing(name)) self$name <- name
-      if (!missing(requires)) {
-        if (length(requires) > 0) {
-          if (!all(sapply(requires, is.ruigi_target)))
-            stop("Invalid ", sQuote("requires"), " for a ruigi node")
-        }
-        self$requires <- requires
-      }
-      ## A runner is the computation that the node must produce
-      ## In code, a runner is a function that has two arguments:
-      ## *requires*, that is a list of ruigi_nodes, and *produces*, which is a ruigi_target
-      ## In practise, this function will be called with the appropriate values by
-      ## the scheduler.
-      if (missing(runner)) stop("Every node must have a ", sQuote("runner"))
-      if (!is.function(runner) | length(runner) != 1)
-        stop("A ", sQuote("runner"), " must be a function")
-      if (! all(names(formals(runner)) %in% c("requires", "target")))
-        stop("A ", sQuote("runner"), " must take ", sQuote("requires"), "and ",
-        sQuote("target"), " as inputs")
-      self$runner <- runner
-      if (missing(target)) stop("Every node must have a ", sQuote("target"))
-      if (!is.ruigi_target(target) | length(target) != 6) {
-        browser()
-        stop("A node must have one and only one target that is a ", sQuote("ruigi_target"))
-      }
-      self$target <- target
-    }
+    initialize = `ruigi_node&initialize`
   )
 )
 
--- R/ruigi_target.R
+++ R/ruigi_target.R
@@ -1,3 +1,34 @@
+`ruigi_target&initialize` <- function(name, requires, write, exists, location) {
+  if (!missing(name)) self$name <- name
+  if (missing(requires)) stop("A target requires an input that will be saved")
+  ## A target must implement two methods: *exists* and *write*
+  ## The scheduler will determine which nodes need to be computed depending on the
+  ## target of every node. Namely, if `target$exists()`` is **TRUE**, then the
+  ## node is assumed to have finished running. Otherwise, `target$write(obj)`
+  ## the node writer should use `target$write(obj)` in the node runner.
+  ## It is a useful abstraction, because it allows to reuse the same node processing
+  ## logic and change the way artifacts are created.
+  if (missing(exists)) stop("Must implement ", sQuote("exists"), " for a target.")
+  if (!is.function(exists) | length(exists) != 1)
+    stop(sQuote("exists"), " must be a function")
+  if (!is.null(formals(exists))) stop(sQuote("exists"), " should not have any inputs")
+  self$exists <- exists
+
+  if (!is.function(read) | length(read) != 1)
+    stop(sQuote("read"), " must be a function")
+  if (!is.null(formals(read))) stop(sQuote("read"), " should not have any inputs")
+  self$read <- read
+
+  if (missing(write)) stop("Must implement ", sQuote("write"), " for a target.")
+  if (!is.function(write) | length(write) != 1)
+    stop(sQuote("write"), " must be a function")
+  if (length(formals(write)) != 1) stop(sQuote("write"), " should have only one input")
+  self$write <- write
+
+  if (missing(location)) stop("Please specify the location for the target")
+  self$location <- location
+}
+
 #' @export
 ruigi_target <- R6::R6Class("ruigi_target",
   public = list(
@@ -11,36 +42,7 @@ ruigi_target <- R6::R6Class("ruigi_target",
     ## since this is the primary interface with the user.
     ## It is important to check early because execution of the node may happen
     ## on another machine, or it will start late and cause errors in a huge batch job.
-    initialize = function(name, requires, write, exists, location) {
-      if (!missing(name)) self$name <- name
-      if (missing(requires)) stop("A target requires an input that will be saved")
-      ## A target must implement two methods: *exists* and *write*
-      ## The scheduler will determine which nodes need to be computed depending on the
-      ## target of every node. Namely, if `target$exists()`` is **TRUE**, then the
-      ## node is assumed to have finished running. Otherwise, `target$write(obj)`
-      ## the node writer should use `target$write(obj)` in the node runner.
-      ## It is a useful abstraction, because it allows to reuse the same node processing
-      ## logic and change the way artifacts are created.
-      if (missing(exists)) stop("Must implement ", sQuote("exists"), " for a target.")
-      if (!is.function(exists) | length(exists) != 1)
-        stop(sQuote("exists"), " must be a function")
-      if (!is.null(formals(exists))) stop(sQuote("exists"), " should not have any inputs")
-      self$exists <- exists
-
-      if (!is.function(read) | length(read) != 1)
-        stop(sQuote("read"), " must be a function")
-      if (!is.null(formals(read))) stop(sQuote("read"), " should not have any inputs")
-      self$read <- read
-
-      if (missing(write)) stop("Must implement ", sQuote("write"), " for a target.")
-      if (!is.function(write) | length(write) != 1)
-        stop(sQuote("write"), " must be a function")
-      if (length(formals(write)) != 1) stop(sQuote("write"), " should have only one input")
-      self$write <- write
-
-      if (missing(location)) stop("Please specify the location for the target")
-      self$location <- location
-    }
+    initialize = `ruigi_target&initialize`
   )
 )
 
