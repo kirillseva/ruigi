10c312567314b2a51d4378612bf16403c330a59d
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-05-13 11:26:38 -0500

    two more classes refactored

--- R/csv_target.R
+++ R/csv_target.R
@@ -1,16 +1,18 @@
+`CSVtarget&initialize` <- function(location, name) {
+  if (missing(location)) stop("Need to specify the filename for ", sQuote("CSVtarget"))
+  self$location <- location
+  self$name <- if (!missing(name)) name else paste(self$name, location)
+
+  self$exists <- function() file.exists(self$location)
+  self$write <- function(obj) write.csv(obj, self$location, row.names = FALSE)
+  self$read <- function() read.csv(self$location)
+}
+
 #' @export
 CSVtarget <- R6::R6Class("CSVtarget",
   inherit = ruigi_target,
   public = list(
     name = paste0("Write to ", sQuote("csv")),
-    initialize = function(location, name) {
-      if (missing(location)) stop("Need to specify the filename for ", sQuote("CSVtarget"))
-      self$location <- location
-      self$name <- if (!missing(name)) name else paste(self$name, location)
-
-      self$exists <- function() file.exists(self$location)
-      self$write <- function(obj) write.csv(obj, self$location, row.names = FALSE)
-      self$read <- function() read.csv(self$location)
-    }
+    initialize = `CSVtarget&initialize`
   )
 )
--- R/s3_target.R
+++ R/s3_target.R
@@ -1,16 +1,18 @@
+`S3target&initialize` <- function(location, name) {
+  if (missing(location)) stop("Need to specify the filename for ", sQuote("CSVtarget"))
+  self$location <- location
+  self$name <- if (!missing(name)) name else paste(self$name, location)
+
+  self$exists <- function() s3mpi::s3exists(self$location)
+  self$write <- function(obj) s3mpi::s3store(obj, self$location)
+  self$read <- function() s3mpi::s3read(self$location)
+}
+
 #' @export
 S3target <- R6::R6Class("S3target",
   inherit = ruigi_target,
   public = list(
     name = paste0("Write to an s3 bucket"),
-    initialize = function(location, name) {
-      if (missing(location)) stop("Need to specify the filename for ", sQuote("CSVtarget"))
-      self$location <- location
-      self$name <- if (!missing(name)) name else paste(self$name, location)
-
-      self$exists <- function() s3mpi::s3exists(self$location)
-      self$write <- function(obj) s3mpi::s3store(obj, self$location)
-      self$read <- function() s3mpi::s3read(self$location)
-    }
+    initialize = `S3target&initialize`
   )
 )
