883c91393389c7984d121316a13619ec04c045f2
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-05-13 10:53:52 -0500

    dependency graph

--- R/pipeline.R
+++ R/pipeline.R
@@ -5,7 +5,36 @@ build_node_graph <- function(nodes) {
   if (! all(sapply(nodes, is.ruigi_node)))
     stop("You must provide a flat list of nodes for luigi to execute.")
 
-  nodes_and_targets <- lapply(nodes, function(node) {
-    list(node = node, target = node$target, requirements = node$requires)
+  nodes <- lapply(nodes, function(node) {
+    list(
+      id = digest::digest(node),
+      node = node,
+      requirements_satisfied = all(sapply(node$requires, function(target) {
+        target$exists()
+      })),
+      finished = node$target$exists()
+    )
   })
+
+  edges <- list()
+  target_id <- function(target) {
+    stopifnot(is.ruigi_target(target))
+    digest::digest(digest::digest(paste0(target$location, class(target))))
+  }
+  ## `O(n^2 * k)`, n - number of nodes, k - maximum number of requirements...
+  ## Pretty sure we can do better
+  lapply(nodes, function(lst) {
+    lapply(nodes, function(other_lst) {
+      ## no edges that start and end in the same node
+      if (identical(lst$id, other_lst$id)) return(NULL)
+      lapply(other_lst$node$requires, function(target) {
+        if (target_id(target) == target_id(lst$node$target)) {
+          edges <<- append(edges, list(list(from = lst$id, to = other_lst$id)))
+        }
+      })
+      NULL
+    })
+  })
+
+  graph <- list(nodes = nodes, edges = edges)
 }
--- tests/testthat/test-build_node_graph.R
+++ tests/testthat/test-build_node_graph.R
@@ -1,12 +1,21 @@
 context('Unit tests for building dependency graph')
 
-test_that('A trivial dependency graph', {
+test_that('A small dependency graph', {
   tmp1 <- tempfile()
   write.csv(data.frame(a=1, b=2, c=3), tmp1, row.names = FALSE)
   tmp2 <- tempfile()
+  tmp3 <- tempfile()
+
+  node0 <- ruigi_node$new(
+    requires = list(),
+    target = CSVtarget$new(tmp3),
+    runner = function(requires, target) {
+      target$write(data.frame(a = 1, d = 4))
+    }
+  )
 
   node1 <- ruigi_node$new(
-    requires = list(CSVtarget$new(tmp1)),
+    requires = list(CSVtarget$new(tmp1), CSVtarget$new(tmp3)),
     target = CSVtarget$new(tmp2),
     runner = function(requires, target) {
       tmpvalue <- requires[[1]]$read()
@@ -14,9 +23,20 @@ test_that('A trivial dependency graph', {
     }
   )
 
-  g <- build_node_graph(list(node1))
-  print(g)
+  node2 <- ruigi_node$new(
+    requires = list(CSVtarget$new(tmp3)),
+    target = CSVtarget$new(tmp2),
+    runner = function(requires, target) {
+      tmpvalue <- requires[[1]]$read()
+      target$write(tmpvalue)
+    }
+  )
+
+  g <- ruigi:::build_node_graph(list(node1, node0, node2))
+  expect_equal(length(g$edges), 2)
+  expect_equal(length(g$nodes), 3)
 
   unlink(tmp1)
   unlink(tmp2)
+  unlink(tmp3)
 })
